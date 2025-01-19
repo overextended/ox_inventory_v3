301e9b3ecbe56fcec5f44049818c15e13b1d1b2f
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2025-01-19 23:51:04 +0100

    fix(common/utils): check if GetParentResourceName is defined

--- src/common/index.ts
+++ src/common/index.ts
@@ -4,4 +4,8 @@ declare const window: {
 
 export const isBrowser = typeof window !== 'undefined';
 export const ResourceContext = isBrowser ? 'web' : IsDuplicityVersion() ? 'server' : 'client';
-export const ResourceName = isBrowser ? window.GetParentResourceName() : (GetCurrentResourceName() ?? 'ox_inventory');
+export const ResourceName = isBrowser
+  ? window.GetParentResourceName
+    ? window.GetParentResourceName()
+    : 'nui-frame-app'
+  : (GetCurrentResourceName() ?? 'ox_inventory');
