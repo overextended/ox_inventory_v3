39c56b928dcda70124f6f35ad40a9c72ec7d929d
Linden <65407488+thelindat@users.noreply.github.com>
2025-01-19 20:35:02 +1100

    refactor(item): minor tweaks to loading db data

--- src/server/item/index.ts
+++ src/server/item/index.ts
@@ -1,10 +1,12 @@
-import { GetRawItem, ItemFactory, ItemMetadata } from '@common/item';
 import { GetItemData, ItemFactory, ItemMetadata, ItemProperties } from '@common/item';
 import { oxmysql } from '@overextended/oxmysql';
 
+function GetItemFromDb(name: string): Promise<ItemProperties> {
+  return oxmysql.prepare(`SELECT * FROM ox_items WHERE name = ?`, [name]);
+}
+
 export async function CreateItem(name: string, metadata?: ItemMetadata) {
-  const Item =
-    GetRawItem(name) ?? ItemFactory(name, await oxmysql.prepare(`SELECT * FROM ox_items WHERE name = ?`, [name]));
+  const Item = GetItemData(name) ?? ItemFactory(name, await GetItemFromDb(name));
 
   return new Item(metadata);
 }
