e34dcad89c481fbc21b4d9638386db57eb0aa873
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2025-01-20 20:49:44 +0100

    feat(web): lower opacity on dragged item original position

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -165,7 +165,8 @@
               data-anchorSlot={item.anchorSlot === index}
               class={cn(
                 'w-full h-full bg-no-repeat bg-contain bg-center absolute top-0 left-0 z-50 bg-black/50',
-                isDragging && 'pointer-events-none'
+                isDragging && 'pointer-events-none',
+                dragSlot === item.anchorSlot && 'opacity-50'
               )}
               style={`background-image: url('${item.icon}');width: ${SLOT_SIZE * item.width - 1}px;height: ${SLOT_SIZE * item.height - SLOT_GAP}px;')`}
             >
