5a8086e709aca2b8a1a8156c17abb0118a4c0941
Linden <65407488+thelindat@users.noreply.github.com>
2025-01-26 11:42:19 +1100

    refactor(common/utils): revert async file loading and fetch
    Will require browser specific functions.

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,23 +1,14 @@
 import { isBrowser, ResourceName } from './';
 
-export async function LoadFile(path: string) {
+export function LoadFile(path: string) {
   if (isBrowser) {
     // todo: fix unable to load in browser
     return;
-
-    const resp = await fetch(`https://${ResourceName}/${path}`, {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-    });
-
-    return await resp.json();
   }
 
   return LoadResourceFile(ResourceName, path);
 }
 
-export async function LoadJsonFile<T = unknown>(path: string) {
-  return JSON.parse(await LoadFile(path)) as T;
+export function LoadJsonFile<T = unknown>(path: string) {
+  return JSON.parse(LoadFile(path)) as T;
 }
