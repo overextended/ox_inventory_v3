dc19874685e92a2b128dbcd84864839f23f901fc
Linden <65407488+thelindat@users.noreply.github.com>
2025-02-04 09:24:20 +1100

    refactor(web/vite): use root as publicDir in dev mode

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,13 +3,16 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  resolve: {
-    alias: {
-      $lib: path.resolve('./src/lib'),
-      '~': path.resolve('../'),
-      '@common': path.resolve('../src/common'),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [svelte()],
+    resolve: {
+      alias: {
+        $lib: path.resolve('./src/lib'),
+        '~': path.resolve('../'),
+        '@common': path.resolve('../src/common'),
+      },
     },
-  },
+    ...(mode === 'development' && { publicDir: '../' }),
+  };
 });
