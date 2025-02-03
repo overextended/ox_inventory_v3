e3b3e982abdc5f86cd9b65ede4dacda8def871ce
Linden <65407488+thelindat@users.noreply.github.com>
2025-02-04 09:23:50 +1100

    fix(common/utils): use dynamic import for LoadJsonFile in the browser

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,14 +1,11 @@
 import { isBrowser, ResourceName } from './';
 
 export function LoadFile(path: string) {
-  if (isBrowser) {
-    // todo: fix unable to load in browser
-    return;
-  }
-
   return LoadResourceFile(ResourceName, path);
 }
 
 export function LoadJsonFile<T = unknown>(path: string) {
-  return JSON.parse(LoadFile(path)) as T;
+  if (!isBrowser) return JSON.parse(LoadFile(path)) as T;
+
+  return import(/* @vite-ignore */`/${path}`);
 }
