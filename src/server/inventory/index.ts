2a38809a498037485bb0ee62c9e5562f76f329f7
Linden <65407488+thelindat@users.noreply.github.com>
2025-02-26 14:54:04 +1100

    fix(server/inventory): load inventory outside of switch

--- src/server/inventory/index.ts
+++ src/server/inventory/index.ts
@@ -29,23 +29,25 @@ export function GetInventory(inventoryId: string | number, data?: string | Parti
   switch (data.type) {
     case 'player':
       break;
+    case 'trunk':
+    case 'glovebox':
+      // todo
+      break;
     case 'drop':
       data.label = `Drop ${+GetHashKey(data.inventoryId)}`;
       data.width = Config.Drop_Width;
       data.height = Config.Drop_Height;
       data.maxWeight = Config.Drop_MaxWeight;
       break;
-    case 'player':
-      const result = db.getInventory(data.inventoryId);
-
-      if (!result) return db.insertInventory(data);
-
-      break;
     default:
       throw new Error(`invalid inventory type ${inventory.type} for id ${inventory.inventoryId}`);
   }
 
-  inventory = new Inventory(Object.assign(data, db.getInventory(data.inventoryId)));
+  const result = db.getInventory(data.inventoryId);
+
+  if (!result) db.insertInventory(data);
+
+  inventory = new Inventory(Object.assign(data, result));
 
   const items = db.getInventoryItems(inventory.inventoryId);
 
