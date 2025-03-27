9f77023ee5c328913fdb5494ffc56f8dd5c9151b
Linden <65407488+thelindat@users.noreply.github.com>
2025-03-27 20:21:14 +1100

    feat(client/clothing): support basegame clothing labels

--- src/client/clothing.ts
+++ src/client/clothing.ts
@@ -1,6 +1,15 @@
 import type { Clothing } from '@common/item/index';
 import { cache, notify, requestModel, sleep } from '@overextended/ox_lib/client';
 
+/**
+ * Working out how to get clothing labels and wouldn't
+ * have been possible without root-cause and the following resources:
+ * - https://github.com/root-cause/v-labels
+ * - https://github.com/root-cause/v-clothingnames
+ * - https://github.com/root-cause/v-decompiled-scripts
+ * - https://gist.github.com/root-cause/3b80234367b0c856d60bf5cb4b826f86
+ */
+
 function GetComponentHash(entity: number, componentId: number, drawableId: number, textureId: number, prop = false) {
   const native = prop ? GetHashNameForProp : GetHashNameForComponent;
   return native(entity, componentId, drawableId, textureId);
@@ -25,6 +34,19 @@ function GetPedComponentLabel(componentHash: number, isProp = false) {
   }
 }
 
+const componentPrefix: Record<number, string> = {
+  1: 'M', // MASK
+  4: 'L', // LEGS
+  6: 'F', // FEET
+  7: 'T', // TEETH
+  11: 'U', // UPPER
+};
+
+const propPrefix: Record<number, string> = {
+  0: 'HT', // HAT
+  1: 'G', // GLASSES
+};
+
 export async function GetClothingLabel(item: Clothing, model = 'mp_m_freemode_01', attempt = 1) {
   await sleep(0);
 
@@ -37,23 +59,27 @@ export async function GetClothingLabel(item: Clothing, model = 'mp_m_freemode_01
 
   if (!ped) return '';
 
-  const native =
-    item.name === 'accessory' ? GetPedPropGlobalIndexFromCollection : GetPedDrawableGlobalIndexFromCollection;
-
+  const isProp = item.name === 'ped_prop';
+  const native = isProp ? GetPedPropGlobalIndexFromCollection : GetPedDrawableGlobalIndexFromCollection;
   const globalId = native(ped, item.componentId, item.collection, item.drawableId);
+  const isMale = model === 'mp_m_freemode_01';
 
   if (globalId === -1) {
     DeleteEntity(ped);
 
-    return GetClothingLabel(
-      item,
-      (model = model === 'mp_m_freemode_01' ? 'mp_f_freemode_01' : 'mp_m_freemode_01'),
-      ++attempt,
-    );
+    return GetClothingLabel(item, (model = isMale ? 'mp_f_freemode_01' : 'mp_m_freemode_01'), ++attempt);
   }
 
-  const hash = GetComponentHash(ped, item.componentId, globalId, item.textureId, item.name === 'accessory');
-  const label = GetPedComponentLabel(hash, item.name === 'accessory');
+  const hash = GetComponentHash(ped, item.componentId, globalId, item.textureId, item.name === 'ped_prop');
+  let label = hash ? GetPedComponentLabel(hash, item.name === 'ped_prop') : 'NULL';
+
+  if (label === 'NULL') {
+    const prefix = (isProp ? propPrefix : componentPrefix)[item.componentId];
+    const gender = isMale ? 'FMM' : 'FMF';
+    const name = `${prefix}_${gender}_${item.drawableId}_${item.textureId}`;
+    label = GetLabelText(name);
+    console.log(name, label, hash);
+  }
 
   DeleteEntity(ped);
 
@@ -68,7 +94,7 @@ export async function UseClothing(item: Clothing) {
       ? 'mp_f_freemode_01'
       : 'mp_m_freemode_01';
 
-  const isProp = item.name === 'accessory';
+  const isProp = item.name === 'ped_prop';
   const { componentId, collection, drawableId, textureId, palleteId } = item;
   const isClothingValid = isProp
     ? GetNumberOfPedCollectionPropDrawableVariations(cache.ped, componentId, collection) > 0
